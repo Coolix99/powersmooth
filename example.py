7c5bc193dbe24d4a38e71f33856571ab69b7416b
Max <kotzmax@gmail.com>
2025-07-01 09:20:02 +0200

    changed example values for smoothing

--- example.py
+++ example.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     x = np.linspace(0, 10, 30)
     y = np.sin(x) + 0.5 * np.random.randn(30)
 
-    smooth_y_general = powersmooth_general(x,y, {1:0.0,2:0.005})
+    smooth_y_general = powersmooth_general(x,y, {1:0.0, 2:1e-6, 3:1e-6})
 
     x_up, y_up, mask_up = upsample_with_mask(x, y, dx=0.1)
     smooth_y_general_up = powersmooth_general(x_up, y_up, {1:0.0, 2:1e-6, 3:1e-6}, mask_up)
